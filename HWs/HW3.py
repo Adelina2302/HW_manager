98dcd3fd2385d9d5875dc4da9ca3caf30ee05154
Adelina2302 <adelina2302.dunina@gmail.com>
2025-09-24 04:44:56 +0000

    added HW_4

--- HWs/HW3.py
+++ HWs/HW3.py
@@ -183,7 +183,7 @@ if llm_vendor == "OpenAI":
         [
             "gpt-4o",        # flagship (default)
             "gpt-4-turbo",
-            "gpt-3.5-turbo"
+            "gpt-5-nano"
         ],
         key="openai_model",
         index=0
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 pg = st.navigation(
     {
         "HWs": [
+            st.Page("HWs/HW4.py", title="HW4"), 
             st.Page("HWs/HW3.py", title="HW3"), 
             st.Page("HWs/HW2.py", title="HW2"),
             st.Page("HWs/HW1.py", title="HW1"),
